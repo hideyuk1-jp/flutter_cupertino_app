b83b06db6a19e2c02635e9f8c2a7d9fd146c0a32
hideyuk1-jp <hh11028+github@gmail.com>
2020-07-08 15:28:27 +0900

    3. Create the initial Cupertino app

---
+++ lib/app.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+
+class CupertinoStoreApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // This app is designed only to work vertically, so we limit
+    // orientations to portrait up and down.
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+    return CupertinoApp(
+      home: CupertinoStoreHomePage(),
+    );
+  }
+}
+
+class CupertinoStoreHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('Cupertino Store'),
+      ),
+      child: SizedBox(),
+    );
+  }
+}
